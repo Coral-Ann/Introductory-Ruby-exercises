7425f1c0efaa8b040963ad4a1b3d16f6992bae7e
Coral <coral.baker@outlook.com>
2022-03-03 17:05:27 +0000

    Refactored shout-at-sam.rb.

--- shout-at-sam.rb
+++ shout-at-sam.rb
@@ -1,8 +1,4 @@
 puts "What is your name? "
-user_input = gets.chomp
+user = gets.chomp
 
-if user_input[0].downcase == "s"
-  puts "#{user_input.upcase}!!! >:("
-else
-  puts "Hi #{user_input.capitalize} :)"
-end
+user.chr.downcase == "s" ? (puts "#{user.upcase}!!! >:(") : (puts "Hi #{user.capitalize} :)")
